2756282f98825aba8ca9fe8044b85172169de850
Sera De La Rosa <sera.delarosa@outlook.com>
2025-02-13 17:04:55 -0500

    All exercises done.

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ const collectibles = [
     { name: 'shiny ball', price: 5.95 },
     { name: 'autographed picture of a dog', price: 10 },
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
-  ];
+];
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
 
 //exercise one
 //method, path, callback function that runs when method and path are combined
@@ -31,6 +42,31 @@ app.get('/collectibles/:index', (req, res) => {
         res.send(`So you want the ${collectibles[req.params.index].name}? For ${collectibles[req.params.index].price}, it can be yours!`);
     }
 });
+
+//exercise four
+app.get('/shoes', (req, res) => {
+    //points to any key value pair after a ? in the url
+    if (req.query['min-price']) {
+        const expensiveShoes = shoes.filter(shoe => shoe.price > req.query['min-price']);
+        res.send(expensiveShoes);
+    } else if (req.query['max-price']) {
+        const cheapShoes = shoe.filter(shoe => shoe.price < req.query['max-price']);
+        res.send(cheapShoes);
+    } else if (req.query.type) {
+        const shoesOfAType = shoes.filter(shoe => shoe.type === req.query.type);
+        res.send(shoesOfAType);
+    } else {
+        res.send(shoes);
+    }
+});
+
+//longform syntax
+// const shoesOfAType = shoes.filter((shoe) => {
+//     shoe.type === req.query.type
+// });
+        
+
+
 
 app.listen(3000, () => {
     console.log('listening');
